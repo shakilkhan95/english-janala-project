7bd707e66bc2e3822d0648e4afcee221a34f2781
shakilkhan95 <th.shakilkhan@gmail.com>
2026-03-05 06:12:10 +0400

    added functionality to fetch and load word details and display them into ui through modal

--- script/index.js
+++ script/index.js
@@ -34,50 +34,49 @@ const displayLessons = (lessons) => {
 const loadLevelWords = (id) => {
   const wordsApiUrl = `https://openapi.programming-hero.com/api/level/${id}`;
   fetch(wordsApiUrl)
-  .then(res => res.json())
-  .then(words => {
+    .then((res) => res.json())
+    .then((words) => {
+      toggleState(); //to add btn-soft class for all buttons by default
+      const clickedBtn = document.getElementById(`lesson-btn-${id}`);
+      clickedBtn.classList.remove("btn-soft");
 
-    toggleState(); //to add btn-soft class for all buttons by default
-    const clickedBtn = document.getElementById(`lesson-btn-${id}`);
-    clickedBtn.classList.remove('btn-soft');
-
-    displayLevelWords(words.data);
-  })
-}
+      displayLevelWords(words.data);
+    });
+};
 
 // function to add btn-soft class for all buttons by default
-const toggleState = () =>{
-    const lessonBtns = document.querySelectorAll('.lesson-btn');
-    lessonBtns.forEach(btn => {
-        btn.classList.add('btn-soft');
-    })
-}
+const toggleState = () => {
+  const lessonBtns = document.querySelectorAll(".lesson-btn");
+  lessonBtns.forEach((btn) => {
+    btn.classList.add("btn-soft");
+  });
+};
 
-// function to display words in ui 
+// function to display words in ui
 const displayLevelWords = (words) => {
-    // get the words container and make it empty 
-    const wordsContainer = document.getElementById("words-container");
-    wordsContainer.innerHTML = '';
+  // get the words container and make it empty
+  const wordsContainer = document.getElementById("words-container");
+  wordsContainer.innerHTML = "";
 
-    //checking if there is no words in any levels.
-    if(words.length === 0){
-        wordsContainer.classList.remove("md:grid-cols-2", "lg:grid-cols-3");
-        const alertDiv = document.createElement('div');
-        alertDiv.innerHTML = `
+  //checking if there is no words in any levels.
+  if (words.length === 0) {
+    wordsContainer.classList.remove("md:grid-cols-2", "lg:grid-cols-3");
+    const alertDiv = document.createElement("div");
+    alertDiv.innerHTML = `
             <div><img class="mx-auto" src="./assets/alert-error.png"></div>
             <p class="text-center text-sm text-[#79716B] font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
             <h3 class="text-center text-3xl text-[#292524] font-bangla mt-5">নেক্সট Lesson এ যান</h3>
         `;
-        wordsContainer.append(alertDiv);
-        return;
-    }
+    wordsContainer.append(alertDiv);
+    return;
+  }
 
-    wordsContainer.classList.add("md:grid-cols-2","lg:grid-cols-3");
+  wordsContainer.classList.add("md:grid-cols-2", "lg:grid-cols-3");
 
-    // loop through the all words and set the values in ui dynamically
-    words.forEach(word => {
-        const cardDiv = document.createElement('div');
-        cardDiv.innerHTML = `
+  // loop through the all words and set the values in ui dynamically
+  words.forEach((word) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
             <!-- word card  -->
             <div class="card bg-white text-white-content h-full pt-5">
                 <div class="card-body items-center text-center">
@@ -85,14 +84,50 @@ const displayLevelWords = (words) => {
                     <p class="text-neutral text-xl font-medium">Meaning / Pronounciation</p>
                     <h2 class="card-title text-[#18181B] text-3xl font-semibold">"${word.meaning ? word.meaning : "!অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "!Pronunciation পাওয়া যায়নি"}"</h2>
                     <div class="flex justify-between w-full mt-3">
-                        <button class="btn btn-square cursor-default"><i class="fa-solid fa-circle-exclamation"></i></button>
+                        <button onclick="loadWordDetails(${word.id})" class="btn btn-square cursor-default"><i class="fa-solid fa-circle-exclamation"></i></button>
                         <button class="btn btn-square cursor-default"><i class="fa-solid fa-volume-high"></i></button>
                     </div>
                 </div>
             </div>
         `;
-        wordsContainer.append(cardDiv);
-    })
+    wordsContainer.append(cardDiv);
+  });
+};
+
+// function to fetch and load word details
+const loadWordDetails = async (id) => {
+  const wordDetailsUrl = `https://openapi.programming-hero.com/api/word/${id}`;
+  const res = await fetch(wordDetailsUrl);
+  const details = await res.json();
+  displayWordDetails(details.data)
+};
+
+// function to display word details in ui by a modal
+const displayWordDetails = (word) => {
+    const wordDetailsContainer = document.getElementById("details-container");
+    wordDetailsContainer.innerHTML = `
+                <h2 class="text-4xl font-semibold text-black">${word.word ? word.word : "!শব্দটি পাওয়া যায়নি"} (<i class="fa-solid fa-microphone-lines"></i> :${word.pronunciation ? word.pronunciation : "!শব্দটি পাওয়া যায়নি"})
+                </h2>
+                <div>
+                    <p class="text-2xl font-semibold text-black">Meaning</p>
+                    <p class="text-2xl font-semibold text-black font-bangla">${word.meaning ? word.meaning : "!অর্থ পাওয়া যায়নি"}</p>
+                </div>
+    
+                <div>
+                    <p class="text-2xl font-semibold text-black">Example</p>
+                    <p class="text-2xl text-black">${word.sentence ? word.sentence : "!বাক্যটি পাওয়া যায়নি"}</p>
+                </div>
+    
+                <div class="space-y-2">
+                    <p class="text-2xl font-medium text-black font-bangla">সমার্থক শব্দ গুলো</p>
+                    <div class="space-x-2">
+                        <span class="rounded bg-blue-200 p-1">${word.synonyms[0] ? word.synonyms[0] : "!সমার্থক শব্দটি পাওয়া যায়নি"}</span>
+                        <span class="rounded bg-blue-200 p-1">${word.synonyms[1] ? word.synonyms[1] : "!সমার্থক শব্দটি পাওয়া যায়নি"}</span>
+                        <span class="rounded bg-blue-200 p-1">${word.synonyms[2] ? word.synonyms[2] : "!সমার্থক শব্দটি পাওয়া যায়নি"}</span>
+                    </div>
+                </div>
+    `;
+    document.getElementById("word_modal").showModal();
 }
 
 loadLessons();
